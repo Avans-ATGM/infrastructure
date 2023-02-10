1a26f8415a4d3f4f0a1677b631162312b7d21d27
Donny Vrins <62692537+Dirowa@users.noreply.github.com>
2023-02-10 17:57:14 +0100

    debugging

--- roles/galaxy_database_backup/templates/copy_galactic_database.sh
+++ roles/galaxy_database_backup/templates/copy_galactic_database.sh
@@ -1,5 +1,5 @@
-rm /data/databases/galaxy_database.sql.gz
-pg_dump galaxy | gzip > /data/databases/galaxy_database.sql.gz
-chmod 777 /data/databases/galaxy_database.sql.gz
+rm /data/databases/.galaxy_database.sql.gz
+pg_dump galaxy | gzip > /data/databases/.galaxy_database.sql.gz
+
 
 info $?
